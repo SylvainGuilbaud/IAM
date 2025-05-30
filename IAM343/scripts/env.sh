dfc3b5fcd3336a0ec3f76ad183f9c44e1cef12a8
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2025-05-30 17:19:13 +0200

    IAM

---
+++ IAM343/scripts/env.sh
@@ -0,0 +1,8 @@
+# export ISC_IAM_IMAGE=intersystems/iam:2.8.1.0-3
+# export ISC_IRIS_URL=http://IAM:SYS@169.254.24.225:52773/api/iam/license
+
+export ISC_IAM_IMAGE=localhost/intersystems/iam-arm64:3.4
+export ISC_IRIS_URL=http://IAM:AAA@10.16.7.77:52774/api/iam/license
+export ISC_IRIS_URL=http://IAM:AAA@172.20.10.3:52773/api/iam/license
+export ISC_IRIS_URL=http://IAM:AAA@10.211.55.2:52773/api/iam/license
+export ISC_IRIS_URL=http://IAM:AAA@192.168.65.1:9092/api/iam/license
