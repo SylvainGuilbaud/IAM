d462bb5b9b3ba016c6f4b17acf450d81c77b4d11
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2025-06-02 18:32:33 +0200

    init

--- IAM343/scripts/4.addRateLimit.sh
+++
@@ -1,8 +0,0 @@
-echo '_format_version: "3.0"
-plugins:
- - name: rate-limiting
-   service: example-service
-   config:
-     minute: 5
-     limit_by: consumer
-     policy: local' | deck gateway apply
--- IAM343/scripts/5.loopToLimit.sh
+++ IAM343/scripts/5.loopToLimit.sh
@@ -1,3 +1,3 @@
-for _ in {1..6}; do 
- curl http://localhost:8000/anything
+for _ in {1..11}; do 
+ curl 'http://localhost:8000/Properties' --user _SYSTEM:SYS
 done
