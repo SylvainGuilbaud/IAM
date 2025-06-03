3a4a2da65ed57834eb1a75e5511a50421e0346bc
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2025-06-03 09:42:19 +0200

    base64

--- IAM343/scripts/passwordBase64.sh
+++ IAM343/scripts/passwordBase64.sh
@@ -1,1 +1,15 @@
-echo -n "_system:SYS" | base64
+### enter username and password, then encode username:password in base64 format
+#!/bin/bash
+
+read -p "Nom d'utilisateur : " username
+read -s -p "Mot de passe : " password
+echo
+
+# Concatène username:password puis encode en base64
+credentials="${username}:${password}"
+encoded=$(echo "$credentials" | base64)
+
+echo "Base64 : $encoded"
+
+
+echo $encoded | base64 --decode
